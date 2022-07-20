217e6fcc8f3c64f5465628f8aee4f522484a3ff1
Lipsa1996 <lipsapriyadarsanidhal@gmail.com>
2022-07-20 14:33:06 +0530

    first commit

---
+++ springboot-aws-pipeline/src/main/java/com/springboot/aws/pipeline/SpringbootAwsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsPipelineApplication.class, args);
+	}
+
+}
---
+++ springboot-aws-pipeline/src/test/java/com/springboot/aws/pipeline/SpringbootAwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
