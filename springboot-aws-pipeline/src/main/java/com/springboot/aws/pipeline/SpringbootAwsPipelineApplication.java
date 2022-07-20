eae45277bc1ccdf73a3ee6ebe8c818b1783c460c
Lipsa1996 <lipsapriyadarsanidhal@gmail.com>
2022-07-20 14:35:49 +0530

    second commit

--- springboot-aws-pipeline/src/main/java/com/springboot/aws/pipeline/SpringbootAwsPipelineApplication.java
+++ springboot-aws-pipeline/src/main/java/com/springboot/aws/pipeline/SpringbootAwsPipelineApplication.java
@@ -2,12 +2,20 @@ package com.springboot.aws.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootAwsPipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAwsPipelineApplication.class, args);
+	}
+	
+	
+	@GetMapping("\test")
+	public String test()
+	{
+		return "Hello world";
 	}
 
 }
